c9984f014222ca745dfd19f5e0b61f919a17d962
Tom Eck <tom.eck@fiserv.com>
2022-05-02 16:40:20 -0400

    Initial Commit

---
+++ iCoffee/iCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCoffeeApp.swift
+//  iCoffee
+//
+//  Created by Eck, Tom on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct iCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
